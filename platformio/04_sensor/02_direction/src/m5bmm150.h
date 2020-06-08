f6b28c3ba0d21bde217704418adf23e0691e40da
Satoru Osawa <osawa@takram.com>
2020-06-08 18:46:02 +0900

    Transplante bmm150 example

--- platformio/04_sensor/02_direction/src/m5bmm150.h
+++ platformio/04_sensor/02_direction/src/m5bmm150.h
@@ -2,18 +2,31 @@
 #define M5_BMM150_H__
 #include <M5Stack.h>
 
+#include "Preferences.h"
 #include "bmm150.h"
 #include "bmm150_defs.h"
 
 class M5Bmm150 {
  public:
-  static int8_t i2c_read(uint8_t dev_id, uint8_t reg_addr, uint8_t *read_data,
+  static int8_t i2cRead(uint8_t dev_id, uint8_t reg_addr, uint8_t *read_data,
+                        uint16_t len);
+  static int8_t i2cWrite(uint8_t dev_id, uint8_t reg_addr, uint8_t *read_data,
                          uint16_t len);
-  static int8_t i2c_write(uint8_t dev_id, uint8_t reg_addr, uint8_t *read_data,
-                          uint16_t len);
-  bool initialization(struct bmm150_dev &dev);
+  int8_t initialize();
+  void calibrate(uint32_t calibrate_time);
+  void loadOffset();
+  void saveOffset();
+  void update();
+  bmm150_mag_data getRawData();
+  float getHeadDirection();
 
  private:
+  Preferences prefs_;
+
+  bmm150_mag_data mag_offset_;
+  bmm150_mag_data mag_max_;
+  bmm150_mag_data mag_min_;
+  struct bmm150_dev dev_;
 };
 
 #endif
--- platformio/04_sensor/02_direction/src/main.cpp
+++ platformio/04_sensor/02_direction/src/main.cpp
@@ -4,29 +4,29 @@
 #include "m5bmm150.h"
 
 M5Bmm150 m5Bmm150;
-struct bmm150_dev dev;
 
 void setup() {
   M5.begin();
   Wire.begin(21, 22, 400000);
 
-  if (!m5Bmm150.initialization(dev)) {
+  if (m5Bmm150.initialize() != BMM150_OK) {
     Serial.println("BMM150 initialization failed.");
     while (1)
       ;
   }
+  m5Bmm150.loadOffset();
   M5.Lcd.setTextSize(2);
 }
 
 void loop() {
-  bmm150_read_mag_data(&dev);
+  m5Bmm150.update();
+  bmm150_mag_data data = m5Bmm150.getRawData();
   M5.Lcd.setCursor(0, 0);
-  M5.Lcd.print("x: ");
-  M5.Lcd.print(dev.data.x);
-  M5.Lcd.print(",y: ");
-  M5.Lcd.print(dev.data.y);
-  M5.Lcd.print(",z: ");
-  M5.Lcd.println(dev.data.z);
-
+  M5.Lcd.printf("Raw data:\n");
+  M5.Lcd.printf("  x: %8.2f\n", data.x);
+  M5.Lcd.printf("  y: %8.2f\n", data.y);
+  M5.Lcd.printf("  z: %8.2f\n", data.z);
+  float dir = m5Bmm150.getHeadDirection();
+  M5.Lcd.printf("direction: %7.2f[deg]", dir);
   delay(100);
 }

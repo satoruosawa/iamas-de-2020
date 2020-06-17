ec12e4650b66266a0730c13593759d878e838850
Satoru Osawa <osawa@takram.com>
2020-06-17 17:23:55 +0900

    Add comment

--- 01_platformio/00_lcd/03_lines/src/main.cpp
+++ 01_platformio/00_lcd/03_lines/src/main.cpp
@@ -4,12 +4,14 @@ void setup() {
   M5.begin();
 
   // iの値が0から4(5未満)まで1つずつ変化
-  // 0 -> 1 -> 2 -> 3 -> 4 end.
+  // 0 -> 1 -> 2 -> 3 -> 4
   for (int i = 0; i < 5; i++) {
+    // 横の線を5本描画
     int y = (i + 1) * 40;
     M5.Lcd.drawLine(10, y, 310, y, WHITE);
   }
   for (int i = 0; i < 7; i++) {
+    // 縦の線を7本描画
     int x = (i + 1) * 40;
     M5.Lcd.drawLine(x, 10, x, 230, WHITE);
   }

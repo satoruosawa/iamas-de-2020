335e8552a6fa275e5332f69b9f165ca484a19072
Satoru Osawa <osawa@takram.com>
2020-06-18 12:53:07 +0900

    Fix speaker
    rename rhythm file name
    
    delete delay
    fix loop algorithm

---
+++ 01_platformio/02_speaker/02_rhythm/src/main.cpp
@@ -0,0 +1,19 @@
+#include <M5Stack.h>
+
+unsigned long prev_update = 0;
+float tempo = 60.0;  // BPM
+
+void setup() { M5.begin(); }
+
+void loop() {
+  M5.update();
+  if (millis() < prev_update + tempo / 60.0 * 1000) return;
+  prev_update = millis();
+  M5.Speaker.tone(3520, 20);
+}
+
+// void loop() {
+//   M5.update();
+//   M5.Speaker.tone(3520, 20);
+//   delay(tempo / 60.0 * 1000);
+// }
